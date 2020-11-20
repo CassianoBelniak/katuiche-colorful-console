6931f58c92572dc93c8377d738211753cee7304c
cassiano belniak <cassi.belniak@gmail.com>
2020-11-20 13:46:51 -0300

    Added project files

---
+++ addons/katuiche-colorful-console/class/print.gd
@@ -0,0 +1,108 @@
+class_name Print, "./icons/class.svg"
+extends Node
+
+const ESCAPE = ''
+const COLOR_RESET = "[0m"
+
+const BLACK = "[0;30m";
+const RED = "[0;31m";
+const GREEN = "[0;32m";
+const YELLOW = "[0;33m";
+const BLUE = "[0;34m";
+const PURPLE = "[0;35m";
+const CYAN = "[0;36m";
+const WHITE = "[0;37m";
+const BLACK_BOLD = "[1;30m";
+const RED_BOLD = "[1;31m";
+const GREEN_BOLD = "[1;32m";
+const YELLOW_BOLD = "[1;33m";
+const BLUE_BOLD = "[1;34m";
+const PURPLE_BOLD = "[1;35m";
+const CYAN_BOLD = "[1;36m";
+const WHITE_BOLD = "[1;37m";
+const BLACK_UNDERLINED = "[4;30m";
+const RED_UNDERLINED = "[4;31m";
+const GREEN_UNDERLINED = "[4;32m";
+const YELLOW_UNDERLINED = "[4;33m";
+const BLUE_UNDERLINED = "[4;34m";
+const PURPLE_UNDERLINED = "[4;35m";
+const CYAN_UNDERLINED = "[4;36m";
+const WHITE_UNDERLINED = "[4;37m";
+const BLACK_BACKGROUND = "[40m";
+const RED_BACKGROUND = "[41m";
+const GREEN_BACKGROUND = "[42m";
+const YELLOW_BACKGROUND = "[43m";
+const BLUE_BACKGROUND = "[44m";
+const PURPLE_BACKGROUND = "[45m";
+const CYAN_BACKGROUND = "[46m";
+const WHITE_BACKGROUND = "[47m";
+const BLACK_BRIGHT = "[0;90m";
+const RED_BRIGHT = "[0;91m";
+const GREEN_BRIGHT = "[0;92m";
+const YELLOW_BRIGHT = "[0;93m";
+const BLUE_BRIGHT = "[0;94m";
+const PURPLE_BRIGHT = "[0;95m";
+const CYAN_BRIGHT = "[0;96m";
+const WHITE_BRIGHT = "[0;97m";
+const BLACK_BOLD_BRIGHT = "[1;90m";
+const RED_BOLD_BRIGHT = "[1;91m";
+const GREEN_BOLD_BRIGHT = "[1;92m";
+const YELLOW_BOLD_BRIGHT = "[1;93m";
+const BLUE_BOLD_BRIGHT = "[1;94m";
+const PURPLE_BOLD_BRIGHT = "[1;95m";
+const CYAN_BOLD_BRIGHT = "[1;96m";
+const WHITE_BOLD_BRIGHT = "[1;97m";
+const BLACK_BACKGROUND_BRIGHT = "[0;100m";
+const RED_BACKGROUND_BRIGHT = "[0;101m";
+const GREEN_BACKGROUND_BRIGHT = "[0;102m";
+const YELLOW_BACKGROUND_BRIGHT = "[0;103m";
+const BLUE_BACKGROUND_BRIGHT = "[0;104m";
+const PURPLE_BACKGROUND_BRIGHT = "[0;105m";
+const CYAN_BACKGROUND_BRIGHT = "[0;106m";
+const WHITE_BACKGROUND_BRIGHT = "[0;107m";
+
+static func clear_console():
+    match OS.get_name():
+        'Windows': print(ESCAPE + 'cls')
+        'OSX': print(ESCAPE + 'clear')
+        _: print(ESCAPE + 'clear')
+
+static func _get_string(what, divider = ""):
+    var string := ""
+    if what is Array:
+        for thing in what:
+            string += String(thing) + divider
+    elif what is String:
+        string = what
+    else:
+        string = String(what)
+    return string
+
+
+static func line(color, what) -> void:
+    set_color(color)
+    print(_get_string(what))
+    reset()
+
+
+static func raw(color, what) -> void:
+    set_color(color)
+    printraw(_get_string(what))
+    reset()
+
+static func s(color, what) -> void:
+    set_color(color)
+    print(_get_string(what, " "))
+    reset()
+
+static func debug(color, what) -> void:
+    set_color(color)
+    print(_get_string(what))
+    print_stack()
+    reset()
+
+static func set_color(color):
+    printraw(color)
+
+static func reset():
+    printraw(ESCAPE + COLOR_RESET)
---
+++ example/example.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+func _ready() -> void:
+    Print.clear_console()
+
+    Print.line(Print.YELLOW, "Hello, in yellow")
+    Print.line(Print.GREEN_BOLD, "Sup, in bold green")
+    Print.line(Print.BLACK + Print.WHITE_BACKGROUND, "Hi, in black text")
+    Print.line(Print.BLUE_UNDERLINED, "How's going, in underlined blue")
+    Print.debug(Print.CYAN + Print.RED_BACKGROUND, "Hello world, in debug")
+    Print.s(Print.CYAN_BRIGHT, ["Hello", "Bom dia", "buen día", "in multiple strings"])
+    Print.line(Print.RED_BOLD, ["this", "also", "works"])
+
+    Print.raw(Print.RED, "r")
+    Print.raw(Print.RED_BRIGHT, "a")
+    Print.raw(Print.YELLOW, "i")
+    Print.raw(Print.GREEN_BOLD, "n")
+    Print.raw(Print.CYAN, "b")
+    Print.raw(Print.BLUE, "o")
+    Print.raw(Print.PURPLE, "w")
+
+    Print.set_color(Print.YELLOW)
+    print("now everthing is in yellow")
+    print("forever")
+    Print.reset()
+    print("or not")
